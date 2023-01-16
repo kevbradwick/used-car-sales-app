92d780a49f6c72f253b0897168b3045ed9248102
Kevin Bradwick <kevinbradwick@gmail.com>
2023-01-16 21:54:09 +0000

    added api endpoint

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
-from flask import Flask, jsonify
+import pathlib as pl
+
+import pandas as pd
+import skops.io as sio
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+with open(str(pl.Path(__file__).parent.resolve() / "model/model.bin"), "rb") as fp:
+    model = sio.loads(fp.read(), trusted=True)
 
 
 @app.get("/")
 def index():
-    return jsonify({})
+    year = request.args.get("year", "")
+    mileage = request.args.get("mileage", "")
+
+    if not all([year, mileage]):
+        return jsonify({"error": "missing args"}), 400
+
+    data = {
+        "year": [int(year)],
+        "mileage": [int(mileage)],
+    }
+
+    df = pd.DataFrame(data=data)
+    predictions = model.predict(df)
+    return jsonify({"prediction": predictions[0]})
